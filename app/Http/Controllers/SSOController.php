21522fdd813f698cf58ede26899438b5e177bed7
crameep <crameep@gmail.com>
2017-08-30 22:14:26 -0500

    First Commit

---
+++ app/Http/Controllers/SSOController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Socialite;
+use Illuminate\Http\Request;
+
+
+use Seat\Eseye\Cache\NullCache;
+use Seat\Eseye\Configuration;
+use Seat\Eseye\Containers\EsiAuthentication;
+use Seat\Eseye\Eseye;
+
+class SSOController extends Controller
+{
+    /**
+     * Redirect the user to the GitHub authentication page.
+     *
+     * @return Response
+     */
+    public function redirectToProvider()
+    {
+        return Socialite::driver('eveonline')->redirect();
+    }
+
+    /**
+     * Obtain the user information from GitHub.
+     *
+     * @return Response
+     */
+    public function handleProviderCallback()
+    {
+        $user = Socialite::driver('eveonline')->user();
+        dd($user);
+        return $user->token;
+    }
+}
